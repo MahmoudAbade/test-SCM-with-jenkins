b427abefc21f4f5060fb7ac8033dfb18dc4bb41d
Mahmoud Abade <mahmood.abadi2000@gmail.com>
2025-11-06 18:48:02 +0200

    fixing a bug

--- scripts.groovy
+++ scripts.groovy
@@ -1,14 +1,19 @@
-def buildApp() {
-    echo "Building branch ${params.BRANCH_VERSION}..."
-    // Build steps here
+// scripts.groovy
+def buildApp(branchVersion) {
+    echo "Building branch ${branchVersion}..."
+    // Placeholder: Clone the correct branch before building
+    // git branch: branchVersion, url: 'YOUR_REPO_URL'
+    // sh './build.sh' 
 }
 
-def testApp() {
-    echo "Running tests on branch ${params.BRANCH_VERSION}..."
-    // Test steps here
+def testApp(branchVersion) {
+    echo "Running tests on branch ${branchVersion}..."
+    // sh './run_tests.sh'
 }
 
-def deployApp() {
-    echo "Deploying branch ${params.BRANCH_VERSION}..."
-    // Deploy steps here
+def deployApp(branchVersion) {
+    echo "Deploying branch ${branchVersion}..."
+    // sh './deploy.sh'
 }
+
+return this
